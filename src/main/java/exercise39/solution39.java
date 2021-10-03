307bc179b86ddcae1d44ede15f7fce389d52451f
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:20:50 -0400

    Pseudocode for exercise 39

---
+++ src/main/java/exercise39/solution39.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 39 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise39;
+
+
+
+public class solution39 {
+
+    /*
+
+    set the instance variables for first name, last name, position, and separation date
+
+    create an employee constructor that allows the string to be called on later
+
+    create a string for first name, last name, position, and separation date that.
+
+    call on the constructor to override the set string values with the ones from the class with all the employee info
+
+    create a class that houses a list of all the given employee first name, last name, position, and separation date.
+    sort the employee names out by their last name
+    print out "Name" + " |"+"Position|"
+    print out "-----" to create the labels and separator.
+
+     */
+}
